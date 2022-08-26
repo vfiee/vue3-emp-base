69ccf1b29727d7ea82b030e0d24d6754f3332f5e
vyron <fiee_vyron@163.com>
2022-08-26 18:41:13 +0800

    feat: init project

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Vue 3 Project</h1>
+    <TableComponent />
+    <JSXComponent />
+    <AppComponent />
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'App'
+}
+</script>
+
+<script lang="ts" setup>
+import TableComponent from '@vue3Base/TableComponent'
+import JSXComponent from '@vue3Base/JSXComponent'
+import AppComponent from '@vue3Base/AppComponent'
+</script>
+
+<style lang="less" scoped></style>
---
+++ src/empShareTypes/@vue3Base.d.ts
@@ -0,0 +1,8 @@
+declare module '@vue3Base/bootstrap' {
+  export {}
+}
+declare module '@vue3Base/main' {}
+declare module '@vue3Base/JSXComponent' {
+  const _default: any
+  export default _default
+}
